a0ee245c09ddc0d0b909585ef487b0b115c5e84a
fliptech <flip@fliptech.com>
2024-10-21 17:42:46 -0700

    adding more VU Meter Driver improvements

--- sources_1/new/VU_MeterDriver.sv
+++ sources_1/new/VU_MeterDriver.sv
@@ -22,8 +22,8 @@
 
 module VU_MeterDriver(
     input clk,
-    input audio_clk_enable,
-    input audio_enable,
+    input audio_clk_enable,         // 96KHz strobe
+    input audio_enable,             // '1' if music playing
     input [7:0] l_audio_signal,     // msb's
     input [7:0] r_audio_signal,     // msb's
     output reg l_VU_pwm,
@@ -32,7 +32,7 @@ module VU_MeterDriver(
 
 parameter NUMBER_OF_AVERAGES = 16;
 
-reg         VU_sample, pwm_ready;
+reg         VU_sample;
 reg [3:0]   accum_cnt = 0;
 reg [5:0]   VU_pwm_clk_cnt = 0;
 reg [23:0]  l_avg_pwm, r_avg_pwm;
@@ -41,7 +41,7 @@ reg [6:0]  l_pwm_duty_cycle, r_pwm_duty_cycle;
 
 // generates average interval (VU sample rate) => 96KHz/NUMBER_OF_AVERAGES = 6KHz
 always @ (posedge clk) begin
-    if (audio_clk_enable) begin
+    if (audio_clk_enable && audio_enable) begin
         if (accum_cnt == (NUMBER_OF_AVERAGES - 1)) begin     
             VU_sample <= 1'b1;      // strobe
             accum_cnt <= 0;
@@ -57,40 +57,32 @@ always @ (posedge clk) begin
     end           
 end // always
 
-//unsigned accum
+//left unsigned accum
 VUmeter_accum left_VU_accum (
     .CLK(clk),              // input wire CLK
     .CE(audio_clk_enable),  // input wire CE
     .BYPASS(VU_sample),     // input wire BYPASS
-    .SCLR(audio_enable),    // input wire SCLR
+    .SCLR(!audio_enable),   // input wire SCLR
     .B({~l_audio_signal[7], l_audio_signal[6:0]}),     // input wire [7 : 0] converted to unsigned
     .Q(l_avg_pwm)           // output wire [15 : 0] Q
 );
 
-//unsigned accum
+//right unsigned accum
 VUmeter_accum right_VU_accum (
     .CLK(CLK),              // input wire CLK
     .CE(audio_clk_enable),  // input wire CE
     .BYPASS(VU_sample),     // input wire BYPASS
-    .SCLR(audio_enable),    // input wire SCLR
+    .SCLR(!audio_enable),   // input wire SCLR
     .B({~r_audio_signal[7], r_audio_signal[6:0]}),     // input wire [7 : 0] converted to unsigned
     .Q(r_avg_pwm)           // output wire [15 : 0] Q
 );
 
-// audio data averager
+// audio data pwm generator
 always @ (posedge clk) begin
-    if (VU_sample) begin        // stb @ 6K cycles
-        pwm_ready = 1'b1;
-    end 
-    else if (VU_pwm_clk_cnt == 63) begin    // stb @ (49.152MHz / 64) cycles/sec => 768,000 cycles/sec
+    if (VU_sample && audio_clk_enable) begin        // stb @ 6K cycles
+        l_pwm_duty_cycle <= l_avg_pwm[15:9];        // load left duty cycle count
+        r_pwm_duty_cycle <= r_avg_pwm[15:9];        // load tight duty cycle count
         VU_pwm_clk_cnt = 0;
-       
-        if (pwm_ready) begin
-            // load duty cycle counter (7bits)
-            l_pwm_duty_cycle <= l_avg_pwm[15:9];
-            r_pwm_duty_cycle <= r_avg_pwm[15:9];
-            pwm_ready = 1'b0;
-        end
     end 
     // samples per duty cycle = 768K / 6K cycles = 128
     else begin
